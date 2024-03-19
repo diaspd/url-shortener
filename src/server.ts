3c2cda58af3752922ea97324fd49827a3f0cf036
Pedro Dias <pdr050203@gmail.com>
2024-03-18 21:06:01 -0300

    add: link route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import fastify from "fastify";
+import { z } from 'zod'
+import { sql } from "./lib/postgres";
 
 const app = fastify()
 
-app.get('/test', () => {
-  return 'Hello World'
+app.post('/links', async (request, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url()
+  })
+  
+  const { code, url } = createLinkSchema.parse(request.body)
+
+  const result = await sql/*sql*/`
+    INSERT INTO short_links (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNING id
+  `
+
+  const link = result[0]
+
+  return reply.status(201).send({ shortLinkId: link.id})
 })
 
 app.listen({ 
