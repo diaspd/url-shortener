6c54841be91547a377efb5dd12f479bf7e504ebc
Pedro Dias <pdr050203@gmail.com>
2024-03-18 21:23:30 -0300

    add: code route

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,65 @@
 import fastify from "fastify";
 import { z } from 'zod'
 import { sql } from "./lib/postgres";
+import postgres from "postgres";
 
 const app = fastify()
 
-app.post('/links', async (request, reply) => {
-  const createLinkSchema = z.object({
+app.get('/:code', async (request) => {
+  const getLinkSchema = z.object({
     code: z.string().min(3),
-    url: z.string().url()
   })
-  
-  const { code, url } = createLinkSchema.parse(request.body)
+
+  const { code } = getLinkSchema.parse(request.params)
 
   const result = await sql/*sql*/`
-    INSERT INTO short_links (code, original_url)
-    VALUES (${code}, ${url})
-    RETURNING id
+    SELECT id, original_url
+    FROM short_links
+    WHERE short_links.code = ${code}
   `
 
-  const link = result[0]
+  return result
+})
 
-  return reply.status(201).send({ shortLinkId: link.id})
+app.get('/api/links', async () => {
+  const result = await sql/*sql*/`
+    SELECT *
+    FROM short_links
+    ORDER BY created_at DESC
+  `
+
+  return result
+})
+
+app.post('/api/links', async (request, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  })
+
+  const { code, url } = createLinkSchema.parse(request.body)
+
+  try {
+    const result = await sql/*sql*/`
+      INSERT INTO short_links (code, original_url)
+      VALUES (${code}, ${url})
+      RETURNING id
+    `
+
+    const link = result[0]
+
+    return reply.status(201).send({ shortLinkId: link.id })
+  } catch (err) {
+    if (err instanceof postgres.PostgresError) {
+      if (err.code === '23505') {
+        return reply.status(400).send({ message: 'Duplicated code!' })
+      }
+    }
+
+    console.error(err)
+
+    return reply.status(500).send({ message: 'Internal error.' })
+  }
 })
 
 app.listen({ 
